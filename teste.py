c6d9a2567cada0e1b2063ca5e9d6b923f96518ed
George Nicolas <149883721+NicolasQueite@users.noreply.github.com>
2023-11-15 23:43:55 -0300

    1

--- teste.py
+++ teste.py
@@ -1,24 +1,21 @@
-# from time import *
-# from tkinter import *
+# Importação das Bibliotecas
+from time import *
+from tkinter import *
 
-# tela = Tk()  # Cria uma janela
-# tela.title('Relógio Digital')  # Titulo da janela
-# tela.geometry("600x100") # Tamanho da janela
-# horario = Label(tela, font=("Arial", 48)) # Exibe um texto na tela
-# horario.pack(anchor="s") # Define a posição do texto
+# Configuração da Janela
+tela = Tk()  # Cria uma janela
+tela.title('Relógio Digital')  # Titulo da janela
+tela.geometry('400x100') # Tamanho da janela
 
-# def relogio(): # Define a função 'relogio'
-#     hora = strftime('%H:%M:%S') # Formata o horario atual e transforma em string 
-#     horario.config(text=hora) # Define que o texto exibido é a variável 'hora'
-#     horario.after(1000, relogio) # Atualiza o texto exibido em horario a cada 1s
-# relogio() # Executa a função
+# Exibição do Relógio
+horario = Label(tela, font=('Arial', 48)) # Exibe um texto na tela
+horario.pack(anchor='s') # Define a posição do texto
 
-# tela.mainloop()  # Mantém a janela aberta
+# Configuração do funcionamento do relógio
+def relogio(): # Define a função 'relogio'
+    hora = strftime('%H:%M:%S') # Formata o horario atual e transforma em string 
+    horario.config(text=hora) # Define que o texto exibido é a variável 'hora'
+    horario.after(1000, relogio) # Atualiza o texto exibido em horario a cada 1s
+relogio() # Executa a função
 
-
-import tkinter
-
-root = tkinter.Tk()
-root.eval('tk::PlaceWindow . center')
-
-root.mainloop()
+tela.mainloop()  # Mantém a janela aberta
