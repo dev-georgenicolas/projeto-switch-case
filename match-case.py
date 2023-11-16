2804700244aa8ecc5261795227c35285d35b4a3e
George Nicolas <149883721+NicolasQueite@users.noreply.github.com>
2023-11-15 23:47:31 -0300

    aaaa

--- match-case.py
+++ match-case.py
@@ -1,4 +1,4 @@
-x = int(input("Escolha qual programa você deseja executar:\n[1]Raiz Quadrada\n[2]Cédulas\n[3]Programa 3\n[4]Programa 4\n"))
+x = int(input("Escolha qual programa você deseja executar:\n[1]Raiz Quadrada\n[2]Cédulas\n[3]Relógio Digital\n[4]Programa 4\n"))
 
 match x:
     case 1:
@@ -61,4 +61,24 @@ match x:
 
                 cedulas = 0
     case 3:
-        
+        # Importação das Bibliotecas
+        from time import *
+        from tkinter import *
+
+        # Configuração da Janela
+        tela = Tk()  # Cria uma janela
+        tela.title('Relógio Digital')  # Titulo da janela
+        tela.geometry('400x100') # Tamanho da janela
+
+        # Exibição do Relógio
+        horario = Label(tela, font=('Arial', 48)) # Exibe um texto na tela
+        horario.pack(anchor='s') # Define a posição do texto
+
+        # Configuração do funcionamento do relógio
+        def relogio(): # Define a função 'relogio'
+            hora = strftime('%H:%M:%S') # Formata o horario atual e transforma em string 
+            horario.config(text=hora) # Define que o texto exibido é a variável 'hora'
+            horario.after(1000, relogio) # Atualiza o texto exibido em horario a cada 1s
+        relogio() # Executa a função
+
+        tela.mainloop()  # Mantém a janela aberta
