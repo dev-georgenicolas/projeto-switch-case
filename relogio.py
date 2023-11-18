f2438e95819a98f3a06d4e3b40555df47bcef6b8
George Nicolas <149883721+NicolasQueite@users.noreply.github.com>
2023-11-18 16:52:54 -0300

    sasd

--- relogio.py
+++ relogio.py
@@ -1,21 +1,17 @@
-# Importação das Bibliotecas
 from time import *
 from tkinter import *
 
-# Configuração da Janela
-tela = Tk()  # Cria uma janela
-tela.title('Relógio Digital')  # Titulo da janela
-tela.geometry('400x100') # Tamanho da janela
+tela = Tk()
+tela.title('Relógio Digital')
+tela.geometry('400x100')
 
-# Exibição do Relógio
-horario = Label(tela, font=('Arial', 48)) # Exibe um texto na tela
-horario.pack(anchor='s') # Define a posição do texto
+horario = Label(tela, font=('Arial', 48))
+horario.pack(anchor='s')
 
-# Configuração do funcionamento do relógio
-def relogio(): # Define a função 'relogio'
-    hora = strftime('%H:%M:%S') # Formata o horario atual e transforma em string 
-    horario.config(text=hora) # Define que o texto exibido é a variável 'hora'
-    horario.after(1000, relogio) # Atualiza o texto exibido em horario a cada 1s
-relogio() # Executa a função
+def relogio():
+    hora = strftime('%H:%M:%S')
+    horario.config(text=hora)
+    horario.after(1000, relogio)
+relogio()
 
-tela.mainloop()  # Mantém a janela aberta
+tela.mainloop()
