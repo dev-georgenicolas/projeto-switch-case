79253a30f30683cbf8fec17eca5d834c9bd555f9
George_Nicolas <149883721+NicolasQueite@users.noreply.github.com>
2023-12-05 16:08:57 -0300

    pdf

---
+++ match_case.py
@@ -0,0 +1,14 @@
+x = int(input("Escolha uma das opções abaixo:\n[1]Futebol\n[2]Calcular Idade\n[3]Login\n[4]Relógio Digital\n[5]Sair\n"))
+
+match x:
+   case 1:
+      import futebol
+         
+   case 2:
+      import jokenpo
+         
+   case 3:
+      import login
+
+   case 4:
+      import relogio
